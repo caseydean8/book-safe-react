85f1ec75b05abfee2e8ecc50a3a1adc2d7bc25fe
Casey Carroll <caseydean8@gmail.com>
2019-10-14 20:44:17 -0400

    Changed api key variable for heroku deployment hopefully

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 require('dotenv').config();
 
-// change to google books api
 const BASEURL = `https://www.googleapis.com/books/v1/volumes?q=`;
-// google api key
-const APIKEY = `&key=${process.env.REACT_APP_API_KEY}`
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
+// google api key
+const APIKEY = `${process.env.REACT_APP_API_KEY}`;
+
 export default {
   search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
+    return axios.get(`${BASEURL }${query}&key=${APIKEY}`);
   }
 };

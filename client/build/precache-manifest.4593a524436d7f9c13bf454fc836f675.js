dfbd7d93c6b0c2f9ed6460e72ef82108df4c9c3d
Casey Carroll <caseydean8@gmail.com>
2022-05-12 18:25:39 -0400

    Created new build folder with yarn build.

---
+++ client/build/precache-manifest.4593a524436d7f9c13bf454fc836f675.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0abf883450dae07aa810",
+    "url": "/static/css/main.3f5747e5.chunk.css"
+  },
+  {
+    "revision": "0abf883450dae07aa810",
+    "url": "/static/js/main.303832a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "24f405b74e371c3f687f",
+    "url": "/static/js/2.5e46dc94.chunk.js"
+  },
+  {
+    "revision": "c83220ff736a9e5e3afa4f8dffb3929c",
+    "url": "/static/media/videoblocks-old-library-shelves_bn3eipdwb_thumbnail-full01.c83220ff.png"
+  },
+  {
+    "revision": "574b6b4966f8ca598016a94d021d4c8f",
+    "url": "/index.html"
+  }
+];

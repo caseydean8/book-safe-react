ce59b865eaeccb1848a8b8fcf6c00ce4f2a6decb
Casey Carroll <caseydean8@gmail.com>
2022-06-22 18:36:51 -0400

    Save book button displays when book is saved

--- client/src/components/AddBookBtn/index.js
+++ client/src/components/AddBookBtn/index.js
@@ -3,7 +3,12 @@ import Button from "../Button";
 import axios from "axios";
 
 class AddBookBtn extends Component {
+  state = {
+    savedStatus: "Save Book",
+  };
+
   postToDB = (props) => {
+    this.setState({ savedStatus: "Saving" });
     var dbBook = {
       title: props.title,
       authors: props.authors,
@@ -14,7 +19,10 @@ class AddBookBtn extends Component {
 
     axios
       .post("/api/books", dbBook)
-      .then(() => console.log(`${props.title} added to database`))
+      .then(() => {
+        console.log(`${props.title} added to database`);
+        this.setState({ savedStatus: "Saved!" });
+      })
       .catch((err) => console.log(err));
   };
 
@@ -27,7 +35,7 @@ class AddBookBtn extends Component {
             this.postToDB(this.props);
           }}
         >
-          Save Book
+        {this.state.savedStatus}
         </Button>
       </div>
     );

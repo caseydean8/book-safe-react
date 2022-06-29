3a09507ca64cbfc17ee6daa2eca468c12db22e39
Casey Carroll <caseydean8@gmail.com>
2022-06-29 12:05:31 -0400

    Fixed margins in search form

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -5,20 +5,22 @@ import "./style.css";
 function SearchForm(props) {
   return (
     <form>
-      <div className="row justify-content-between">
-        <div className="col-8">
-          <input
-            onChange={props.handleInputChange}
-            value={props.search}
-            name="search"
-            type="text"
-            className="form-control"
-            placeholder="Search for a Book"
-            id="search"
-          />
-        </div>
-        <div className="col search-btn-col">
-          <Button onClick={props.handleFormSubmit}>Search</Button>
+      <div className="row with-margin">
+        <div className="col-lg-12">
+          <div className="input-group">
+            <input
+              onChange={props.handleInputChange}
+              value={props.search}
+              name="search"
+              type="text"
+              className="form-control"
+              placeholder="Search for a Book"
+              id="search"
+            />
+            <span className="input-group-btn">
+              <Button onClick={props.handleFormSubmit}>Search</Button>
+            </span>
+          </div>
         </div>
       </div>
     </form>

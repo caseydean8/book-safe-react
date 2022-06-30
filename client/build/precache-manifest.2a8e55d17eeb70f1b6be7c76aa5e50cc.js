29df3bff32ce6cc2b77c8a44e6bc64aee5800c53
Casey Carroll <caseydean8@gmail.com>
2022-06-30 11:49:44 -0400

    Change NoThumbnail image to string for heroku

---
+++ client/build/precache-manifest.2a8e55d17eeb70f1b6be7c76aa5e50cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "431cb90f0aca7106e726",
+    "url": "/static/css/main.a9a4bfc9.chunk.css"
+  },
+  {
+    "revision": "431cb90f0aca7106e726",
+    "url": "/static/js/main.e67aa98b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b331b2a49dce5f7d88cf",
+    "url": "/static/js/2.08ab3ad4.chunk.js"
+  },
+  {
+    "revision": "1628aa2a995c5ab966b4d65cfbf76464",
+    "url": "/static/media/no-img.1628aa2a.webp"
+  },
+  {
+    "revision": "c83220ff736a9e5e3afa4f8dffb3929c",
+    "url": "/static/media/videoblocks-old-library-shelves_bn3eipdwb_thumbnail-full01.c83220ff.png"
+  },
+  {
+    "revision": "18660ab40328d128b3887f8ad3bf3923",
+    "url": "/index.html"
+  }
+];
--- client/src/components/ListItem/index.js
+++ client/src/components/ListItem/index.js
@@ -10,14 +10,14 @@ function ListItem(props) {
       authors.push(<h4 key={index}>{author}</h4>);
     });
   }
-  console.log(`props.image`)
-  console.log(props.image)
+  console.log(`props.image`);
+  console.log(props.image);
   console.log(`noThumbnail`);
   console.log(NoThumbnail);
-  
-  let showImg = NoThumbnail
+
+  let showImg = `${NoThumbnail}`;
   if (props.image) {
-    showImg = props.image
+    showImg = props.image;
   }
   return (
     <div>
